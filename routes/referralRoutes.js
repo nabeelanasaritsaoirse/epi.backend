cbf11cdf34ff4f9c7a77f071adcef5c21935f09c
nishantitsaoirse-head <nishant.it.saoirse@gmail.com>
2026-04-29 10:01:29 +0530

    Update referral routes

--- routes/referralRoutes.js
+++ routes/referralRoutes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const referralController = require("../controllers/referralController");
 const auth = require("../middlewares/auth");
